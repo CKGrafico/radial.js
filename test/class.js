a02b9c17bf13c0a344616f7582ad23512fa84d4f
Quique Fdez Guerra <quique@ckgrafico.com>
2014-05-22 10:39:14 +0200

    Add test Calculate positions

--- test/class.js
+++ test/class.js
@@ -8,15 +8,9 @@ describe('Exists Radial class', function(){
 	
 });
 
-var myItems = [
-	{
-		html: 'testing',
-	}
-];
-
-var radial = new Radial(myItems);
-
 describe('Pass some items to the constructor', function(){
+	var myItems = [{html: 'testing'}];
+	var radial = new Radial(myItems);
 	
 	it('and these items are in array', function(){
 		expect(radial.count()).toBeDefined();
@@ -33,7 +27,10 @@ describe('Pass some items to the constructor', function(){
 
 });
 
-describe('Manage the list',function(){
+describe('Manage the list', function(){
+	var myItems = [{html: 'testing'}];
+	var radial = new Radial(myItems);
+	
 	it('and can add new item', function(){
 		expect(radial.count()).toEqual(1);
 		radial.add({html: 'second test'});
@@ -56,6 +53,36 @@ describe('Manage the list',function(){
 		expect(radial.count()).toEqual(4);
 		radial.remove(3);
 		expect(radial.count()).toEqual(3);
+	});
+});
+
+describe('Calculate positions', function(){
+	var myItems = [{html: 1},{html: 2},{html: 3},{html: 4}];
+	var radial = new Radial(myItems);
+
+	it('and 4 items paint a cross', function(){
+		expect(radial.count()).toEqual(4);
+		expect(radial.getAlfa(0)).toEqual(0);
+		radial.calc();
+		expect(radial.getAlfa(0)).toEqual(0);
+		expect(radial.getAlfa(1)).toEqual(90);
+		expect(radial.getAlfa(2)).toEqual(180);
+		expect(radial.getAlfa(3)).toEqual(270);
+	});
+	
+	it('and 8 items paint a circle', function(){
+		var myItems2 = [{html: 5},{html: 6},{html: 7},{html: 8}];
+		radial.add(myItems2);
+		expect(radial.count()).toEqual(8);
+		radial.calc();
+		expect(radial.getAlfa(0)).toEqual(0);
+		expect(radial.getAlfa(1)).toEqual(45);
+		expect(radial.getAlfa(2)).toEqual(90);
+		expect(radial.getAlfa(3)).toEqual(135);
+		expect(radial.getAlfa(4)).toEqual(180);
+		expect(radial.getAlfa(5)).toEqual(225);
+		expect(radial.getAlfa(6)).toEqual(270);
+		expect(radial.getAlfa(7)).toEqual(315);
 	});
 });
 
